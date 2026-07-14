d638ccb459d2ab68bb78625b6f69a87cd0ed9678
ptrarizqyramadhan-code <ptrarizqyramadhan@gmail.com>
2026-07-14 10:27:56 +0700

    penambahan int panjang,lebar, void input dan int luas persegi

---
+++ .vscode/function_with_parameter.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int panjang, lebar;
+
+void input ()
+{
+    cout << "Masukkan panjang : ";
+    cin >> panjang;
+    cout << "Masukkan lebar : ";
+    cin >> lebar;       
+}
+
+int luaspersegi (int a, int b)
+{
+    return a * b;
+}
