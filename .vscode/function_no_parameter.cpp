d091edef41e3a9dfc7e0515e3a76baf2ef8e0cf4
ptrarizqyramadhan-code <ptrarizqyramadhan@gmail.com>
2026-07-14 10:20:23 +0700

    penmabhan void input

--- .vscode/function_no_parameter.cpp
+++ .vscode/function_no_parameter.cpp
@@ -2,3 +2,11 @@
 using namespace std;
 
 int panjang,lebar;
+
+void input ()
+{
+    cout << "Masukkan panjang : ";
+    cin >> panjang;
+    cout << "Masukkan lebar : ";
+    cin >> lebar;       
+}
